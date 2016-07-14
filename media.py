39f02c39e5c917777bf995fb0c26d0364a303814
harikrish narasiman <Harikrishnan_Narasimhan@cable.comcast.com>
2016-07-14 11:11:19 -0400

    doc strings

--- media.py
+++ media.py
@@ -1,14 +1,14 @@
 import webbrowser
-""" Movie class to hold information about any movie with the following attributes
-
-    Attributes:
-        title (str): Movie original title
-        story_line (str): Long string explaining the storyline of the movie
-        poster_image_url(str): url string linking to an actual image on the internet
-        trailer_youtube_url(str): youtube url pointing to trailer of the movie
-"""
 
 class Movie():
+    """Movie class to hold information about any movie with the following attributes
+
+        Attributes:
+            title (str): Movie original title
+            story_line (str): Long string explaining the storyline of the movie
+            poster_image_url (str): url string linking to an actual image on the internet
+            trailer_youtube_url (str): youtube url pointing to trailer of the movie
+    """
 
     VALID_RATINGS = ['R','PG','PG13','NR']
     def __init__(self,title, story_line, poster_image, trailer):
